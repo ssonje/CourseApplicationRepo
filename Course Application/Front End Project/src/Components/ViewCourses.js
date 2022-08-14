e190057fb3d842a8544f9a0dba41345d0d90df63
ssonje <ssonje99@gmail.com>
2022-08-14 20:29:02 +0530

    Added Get all courses from the Database utils

--- Course Application/Front End Project/src/Components/ViewCourses.js
+++ Course Application/Front End Project/src/Components/ViewCourses.js
@@ -1,27 +1,15 @@
 import React, { useState} from "react";
 import Course from './Course';
-import base_url from "./../api/bootapi";
-import axios from "axios";
-import { toast } from "react-toastify";
 import { useDocumentTitle } from '../Helpers/useDocumentTitleHook';
 import { useFetchData } from "../Helpers/useFetchDataHook";
+import { GetAllCoursesFromDatabase } from "../Database Service Components/GetAllCoursesFromDatabaseUtil";
 
 function ViewCourses() {
     const [courses, setCourses] = useState([]);
 
     // Get all the courses from the server
     const getAllCourses = () => {
-        axios.get(`${base_url}/courses`).then(
-            (response) => {
-                // Successfully get all the data from the server
-                toast.success("All courses has been loaded!!!");
-                setCourses(response.data);
-            },
-            (error)=> {
-                // Error while fetching the data
-                toast.error("Something went wrong!!!");
-            }
-        )
+        GetAllCoursesFromDatabase(setCourses);
     };
 
     // Call the useDocumentTitle to set the document title and Skip initial execution of useEffect
