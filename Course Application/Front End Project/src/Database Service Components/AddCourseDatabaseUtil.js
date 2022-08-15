7a2036c3561c20b52117d48a96172f1d3235f68b
ssonje <ssonje99@gmail.com>
2022-08-15 17:23:17 +0530

    Adding the documentation for add course

--- Course Application/Front End Project/src/Database Service Components/AddCourseDatabaseUtil.js
+++ Course Application/Front End Project/src/Database Service Components/AddCourseDatabaseUtil.js
@@ -2,11 +2,19 @@ import axios from "axios";
 import base_url from "../Constants/serverBaseURL";
 import { toast } from "react-toastify";
 
+/**
+ * @function
+ * `AddCourseDatabaseUtil` provides the functionality to post data into the database by using the `axios`.
+ * @param {navigate} navigate
+ * Navigate to the destination where we want to go after successfully adding course to the database.
+ * @param {course} course
+ * This course will be added into the database.
+ */
 export const AddCourseDatabaseUtil = (navigate, course) => {
     return (
         axios.post(`${base_url}/courses`, course).then(
             (response) => {
-                // Successfully post the data to the server
+                // Successfully post the data to the database
                 toast.success("Course has been added to the database!!!");
 
                 // Navigate to the view courses pages after course has been successfully added to the system
