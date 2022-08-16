26e7bc934be8c7d1c0beb06d9958751196d4cb22
ssonje <ssonje99@gmail.com>
2022-08-16 09:25:34 +0530

    Added Documentation for the Get All Courses

--- Course Application/Front End Project/src/Database Service Components/GetAllCoursesFromDatabaseUtil.js
+++ Course Application/Front End Project/src/Database Service Components/GetAllCoursesFromDatabaseUtil.js
@@ -2,16 +2,22 @@ import axios from "axios";
 import base_url from "../Constants/serverBaseURL";
 import { toast } from "react-toastify";
 
-export const GetAllCoursesFromDatabase = (setCourses) => {
+/**
+ * @function
+ * `GetAllCoursesFromDatabaseUtil` provides the functionality to get all courses related data from the database by using the `axios`.
+ * @param {setCourses} setCourses
+ * This function used to update the existing courses list with the new course list.
+ */
+export const GetAllCoursesFromDatabaseUtil = (setCourses) => {
     return (
         axios.get(`${base_url}/courses`).then(
             (response) => {
-                // Successfully get all the data from the server
+                // Successfully get all the data from the database
                 toast.success("All courses has been loaded!!!");
                 setCourses(response.data);
             },
             (error) => {
-                // Error while fetching the data from the server
+                // Error while fetching the data from the database
                 toast.error("Something went wrong!!!");
             }
         )
